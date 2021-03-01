51691d4706dd7736e5f9078f3c2a7aa13f040e1a
haiso-yen <58282037abc123@gmail.com>
2021-03-01 14:03:06 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>header</h1>
+      <p>paragraph</p>
+      <div className="nav">
+        <ul>
+          <li><a href="#">choice A</a></li>
+          <li><a href="#">choice B</a></li>
+          <li><a href="#">choice C</a></li>
+          <li><a href="#">choice D</a></li>
+          <li><a href="#">choice X</a></li>
+        </ul>
+      </div>
+      <div className="container">
+          <section>
+            <article>
+            </article>
+          </section>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
